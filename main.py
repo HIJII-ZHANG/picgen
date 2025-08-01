890797a3b32865da519a4a586541f9292ba0e482
HIJII_ZHANG <zhanghaichuan@mail.ustc.edu.cn>
2025-08-01 16:32:06 +0800

    feat: update main loop

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from new_pic import get_part
 from new_pic import new_pic_data
+from new_pic import new_content
 
 save_path = "form_out.jpg"
 pic_path = "form.jpg"
 json_path = "form_new.json"
 
 def main():
+    
     click_path = "form_clicked.jpg"
     clicker = get_part.CheckboxClicker()
     clicker.tick_from_json_click(pic_path, json_path, click_path)
@@ -18,6 +20,12 @@ def main():
         anno_path=json_path,
         out_path=save_path,
     )
+
+    #content_processor = new_content.content_processor(json_path=json_path, save_path)
+    #content_processor.new_content_vl(num=1)
+    #content_processor.optional_to_handwritten()
+    #content_processor.flush_optional()
+    
     print(f"Saved: {save_path}")
 
 
